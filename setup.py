9f6532fba670667bfa464e6bb2e4fdf6e729164b
Christoph Rieke <christoph.k.rieke@up42.com>
2021-06-19 17:08:02 +0200

    long description

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ parent_dir = Path(__file__).resolve().parent
 
 setuptools.setup(
     name="streamlit-keplergl",
-    version="0.0.1",
+    version="0.0.2",
     author="Christoph Rieke",
     author_email="",
     description="Streamlit Component for rendering kepler.gl maps",
-    long_description="",
-    long_description_content_type="text/plain",
+    long_description=parent_dir.joinpath("README.md").read_text(),
+    long_description_content_type="text/markdown",
     url="https://github.com/chrieke/streamlit-keplergl",
     license="MIT",
     packages=setuptools.find_packages(exclude=("tests", "docs", "examples")),
