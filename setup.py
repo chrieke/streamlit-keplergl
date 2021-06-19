34536dbb4eca65f4026c466240eafa554c41c2a0
Christoph Rieke <christoph.rieke@up42.com>
2021-06-19 15:07:27 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit_keplergl",
+    version="0.0.1",
+    author="Christoph Rieke",
+    author_email="",
+    description="Streamlit Component for rendering kepler.gl maps",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="https://github.com/chrieke/streamlit-keplergl",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=["streamlit>=0.79", "keplergl"],
+)
