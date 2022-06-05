05a65a63fd4883970b80206ee9c0ed95b3c2bbf5
Christoph Rieke <christoph.k.rieke@gmail.com>
2022-06-05 23:25:23 +0200

    add visualiaztion kwargs directly

--- streamlit_keplergl/__init__.py
+++ streamlit_keplergl/__init__.py
@@ -5,12 +5,15 @@ from keplergl import KeplerGl
 
 
 def keplergl_static(
-    fig: KeplerGl, height: Optional[int] = None, width: Optional[int] = None, scrolling=False
+    fig: KeplerGl,
+    height: Optional[int] = None,
+    width: Optional[int] = None,
+    center_map=False,
+    read_only=False,
 ) -> components.html:
     """
-    Renders `keplergl.KeplerGl` map figure in a Streamlit app. This method is
-    a static Streamlit Component, meaning, no information is passed back from
-    KeplerGL on browser interaction.
+    Renders `keplergl.KeplerGl` map figure in a Streamlit app. This method is a static Streamlit Component,
+    thus no information is passed back from KeplerGL on browser interaction.
 
     Args:
         fig: `keplergl.KeplerGl` map figure.
@@ -18,7 +21,8 @@ def keplergl_static(
             setting of the KeplerGl.keplergl figure object.
         width: Fixed pixel width of the map, optional. By default the width of the map adjusts
             to the streamlit layout option, e.g. also when used inside a column or container etc.
-        kwargs: Add any argument of KeplerGl.save_to_html() (data, config, read_only).
+        center_map: if center_map is True, the bound of the map will be updated acoording to the current map data
+        read_only: if read_only is True, hide side panel to disable map customization
 
     Example:
         ```python
@@ -27,12 +31,12 @@ def keplergl_static(
         ```
     """
     try:
-        html = fig._repr_html_()
+        html = fig._repr_html_(center_map=center_map, read_only=read_only)
     except AttributeError:
-        raise TypeError("fig argument has to be a keplergl map object of type keplergl.KeplerGl!")
+        raise TypeError(
+            "fig argument has to be a keplergl map object of type keplergl.KeplerGl!"
+        )
 
     if height is None:
         height = fig.height
-    return components.html(
-        html, height=height + 10, width=width, scrolling=scrolling
-    )
+    return components.html(html, height=height + 10, width=width)
