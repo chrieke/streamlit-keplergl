6a11040c9a71bde30dface2c5f02fed517692e25
Christoph Rieke <christoph.k.rieke@gmail.com>
2022-12-23 22:45:59 +0100

    add thrid datapoint

--- examples/streamlit-keplergl-example-with-data.py
+++ examples/streamlit-keplergl-example-with-data.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 df = pd.DataFrame(
     {
-        "City": ["San Francisco", "San Jose"],
-        "Latitude": [37.77, 37.33],
-        "Longitude": [-122.43, -121.89],
+        "City": ["San Francisco", "San Jose", "Palo Alto"],
+        "Latitude": [37.77, 37.33, 37.44],
+        "Longitude": [-122.43, -121.89, -122.14],
     }
 )
 
